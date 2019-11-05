4c07ef0d84fbbf8bdb6cbb1242a0dfcd40c73dbc
Garett Miller <millergarett@gmail.com>
2019-11-04 22:33:12 -0500

    Fixed files with correct votes

--- ballots.py
+++ ballots.py
@@ -30,8 +30,9 @@ def get_ballots_and_candidates(file_address, num_votes):
         if is_ballot:
             ballot = ballot.replace('\n', '')
             votes = [int(i) for i in ballot.split(' ')][1:-1]
-            ballots[ballot_num] = votes
-            ballot_num += 1
+            if votes:
+                ballots[ballot_num] = votes
+                ballot_num += 1
         else:
             candidate = ballot.replace('"', '').replace('\n', '')
             candidates.insert(len(candidates), candidate)
@@ -48,7 +49,7 @@ def transform_matrix(ballots, num_candidates):
 
     for ballot_num in range(len(ballots)):
         ballot = ballots[ballot_num]
-        value = 25
+        value = num_candidates
         for vote_num in range(len(ballot)):
             transformed_ballots[ballot_num][ballot[vote_num] - 1] = value
             value -= 1
@@ -89,15 +90,40 @@ def get_clustered_vote_counts(cluster1, cluster2, cluster3, num_candidates, num_
     return votes1, votes2, votes3
 
 
+def vote_counts(vote_cast, counts, ballots, ballot_index, i, max_value):
+    return len(ballots[ballot_index]) > i and np.array(counts[ballots[ballot_index][i] - 1]).sum() < max_value and not vote_cast[ballot_index]
+
+
+def run_stv(ballots, clusters, num_votes, num_candidates):
+    max_value = len(ballots)/num_candidates
+    counts = np.zeros((num_candidates, 3))
+    win_order = np.zeros(num_candidates)
+    vote_cast = [False for i in range(len(ballots))]
+    for round in range(num_votes):
+        for ballot_index in range(len(ballots)):
+            if vote_counts(vote_cast, counts, ballots, ballot_index, round, max_value):
+                candidate_index = ballots[ballot_index][round] - 1
+                counts[candidate_index][clusters[ballot_index]] += 1
+                votes_received = np.array(counts[candidate_index]).sum()
+                if votes_received < max_value and win_order[candidate_index] == 0:
+                    win_order[candidate_index] = int(round) + 1
+                vote_cast[ballot_index] = True
+
+    return counts, win_order
+
+
 momentum_slate = [[11, 44, 18, 15, 47, 39, 13, 31, 34, 8, 5, 38, 37, 2, 4, 26, 46, 43, 19, 32, 48, 6, 7, 25, 21, 3, 33, 50, 16, 17]]
 left_unity_slate_dep = [[27, 14, 12, 24, 23, 30, 20, 49, 42, 35, 41, 51]]
 left_unity_slate = [[24, 12, 23, 30, 27, 14, 20, 28, 49, 35, 42, 41, 51, 36, 9, 1, 29, 40]]
 
 num_candidates = 51
-num_votes = 25
 
-ballots, candidates = get_ballots_and_candidates("./include/ballots10 (1).txt", num_votes)
+ballots, candidates = get_ballots_and_candidates("./include/ballots10 (1).txt", num_candidates)
 transformed_ballots = transform_matrix(ballots, num_candidates)
+print(transformed_ballots)
+
+
+
 reduced_array, pca = do_pca(transformed_ballots)
 cluster_labels = KMeans(n_clusters=3, n_init=100).fit_predict(reduced_array)
 
@@ -114,10 +140,22 @@ vote_cluster1, vote_cluster2, vote_cluster3 = split_clusters(transformed_ballots
 
 #outputs.plot_pca_ballots(np.array(cluster1), np.array(cluster2), np.array(cluster3), rotated_slates)
 
-votes1, votes2, votes3 = get_clustered_vote_counts(np.array(vote_cluster1), np.array(vote_cluster2), np.array(vote_cluster3), num_candidates, num_votes)
+#votes1, votes2, votes3 = get_clustered_vote_counts(np.array(vote_cluster1), np.array(vote_cluster2), np.array(vote_cluster3), num_candidates, num_candidates)
 #print(votes1)
-outputs.plot_vote_counts(votes1, votes2, votes3, candidates)
+#outputs.plot_vote_counts(votes1, votes2, votes3, candidates)
 
 #outputs.print_slates(candidates, momentum_slate, left_unity_slate)
 
 #outputs.print_pca_components(pca)
+
+# for ballot in transformed_ballots:
+#     print(ballot[16])
+    #if len(ballot) > 5 and ballot[16] == 16:
+        #print(ballot)
+# counts, win_order = run_stv(ballots, cluster_labels, num_votes, num_candidates)
+# x = 0
+# for i in range(num_candidates):
+#     if win_order[i] == 1:
+#         x += 1
+#     print(candidates[i] + ': ' + str('Round ' + str(win_order[i])))
+# print(x)
--- outputs.py
+++ outputs.py
@@ -1,4 +1,3 @@
-import numpy as np
 import matplotlib.pyplot as plt
 
 
@@ -33,38 +32,38 @@ def plot_important_components(pca, candidates):
 
 
 def plot_pca_ballots(cluster1, cluster2, cluster3, rotated_slates):
-    group1, = plt.plot(cluster1[:, :1], cluster1[:, 1:2], 'bo')
+    group1, = plt.plot(cluster1[:, :1], cluster1[:, 1:2], 'yo')
     group2, = plt.plot(cluster2[:, :1], cluster2[:, 1:2], 'ro')
-    group3, = plt.plot(cluster3[:, :1], cluster3[:, 1:2], 'yo')
+    group3, = plt.plot(cluster3[:, :1], cluster3[:, 1:2], 'bo')
     group4, = plt.plot(rotated_slates[:, :1], rotated_slates[:, 1:2], 'g*')
-    plt.legend((group1, group2, group3, group4), ("Unaligned", "Left Unity", "Momentum", "Slate Voting Guides"))
+    plt.legend((group1, group2, group3, group4), ("Unaligned", "Momentum", "Left Unity", "Slate Voting Guides"))
     plt.title("Voter Plot Principal Axes 1 and 2")
     plt.xlabel("Principal Axis 1: Caucus")
     plt.ylabel("Principal Axis 2: Buxmont")
     plt.savefig('output_files/vote_plots/PCA12.png')
     plt.clf()
 
-    group1, = plt.plot(cluster3[:, :1], cluster3[:, 2:3], 'yo', label="Momentum")
-    group2, = plt.plot(cluster1[:, :1], cluster1[:, 2:3], 'bo', label="Unaligned")
-    group3, = plt.plot(rotated_slates[:, :1], rotated_slates[:, 2:3], 'g*', label="Slate Voting Guides")
-    group4, = plt.plot(cluster2[:, :1], cluster2[:, 2:3], 'ro', label="Left Unity")
-    plt.legend((group1, group2, group3, group4), ("Unaligned", "Left Unity", "Momentum", "Slate Voting Guides"))
+    group1, = plt.plot(cluster1[:, :1], cluster1[:, 2:3], 'yo')
+    group2, = plt.plot(cluster2[:, :1], cluster2[:, 2:3], 'ro')
+    group3, = plt.plot(cluster3[:, :1], cluster3[:, 2:3], 'bo')
+    group4, = plt.plot(rotated_slates[:, :1], rotated_slates[:, 2:3], 'g*', label="Slate Voting Guides")
+    plt.legend((group1, group2, group3, group4), ("Unaligned", "Momentum", "Left Unity", "Slate Voting Guides"))
     plt.title("Voter Plot Principal Axes 1 and 3")
     plt.xlabel("Principal Axis 1: Caucus")
     plt.ylabel("Principal Axis 3: Uncast Votes")
-    plt.legend(["Unaligned", "Left Unity", "Momentum", "Slate Voting Guides"])
+    #plt.legend(["Unaligned", "Left Unity", "Momentum", "Slate Voting Guides"])
     plt.savefig('output_files/vote_plots/PCA13.png')
     plt.clf()
 
-    group1, = plt.plot(cluster1[:, :1], cluster1[:, 5:6], 'bo', label="Unaligned")
-    group2, = plt.plot(cluster2[:, :1], cluster2[:, 5:6], 'ro', label="Left Unity")
-    group3, = plt.plot(cluster3[:, :1], cluster3[:, 5:6], 'yo', label="Momentum")
+    group1, = plt.plot(cluster1[:, :1], cluster1[:, 5:6], 'yo')
+    group2, = plt.plot(cluster2[:, :1], cluster2[:, 5:6], 'ro')
+    group3, = plt.plot(cluster3[:, :1], cluster3[:, 5:6], 'bo')
     group4, = plt.plot(rotated_slates[:, :1], rotated_slates[:, 5:6], 'g*', label="Slate Voting Guides")
-    plt.legend((group1, group2, group3, group4), ("Unaligned", "Left Unity", "Momentum", "Slate Voting Guides"))
+    plt.legend((group1, group2, group3, group4), ("Unaligned", "Momentum", "Left Unity", "Slate Voting Guides"))
     plt.title("Voter Plot Principal Axes 1 and 6")
     plt.xlabel("Principal Axis 1: Caucus")
     plt.ylabel("Principal Axis 6: Identity")
-    plt.legend(["Unaligned", "Left Unity", "Momentum", "Slate Voting Guides"])
+    #plt.legend(["Unaligned", "Left Unity", "Momentum", "Slate Voting Guides"])
     plt.savefig('output_files/vote_plots/PCA16.png')
     plt.clf()
 
@@ -81,9 +80,9 @@ def print_slates(candidates, momentum_slate, left_unity_slate):
 
 def plot_vote_counts(cluster1, cluster2, cluster3, candidates):
     for candidate_index in range(len(candidates)):
-        candidate_vote_counts1 = cluster1[candidate_index, 1:26]
-        candidate_vote_counts2 = cluster2[candidate_index, 1:26]
-        candidate_vote_counts3 = cluster3[candidate_index, 1:26]
+        candidate_vote_counts1 = cluster1[candidate_index, 1:52]
+        candidate_vote_counts2 = cluster2[candidate_index, 1:52]
+        candidate_vote_counts3 = cluster3[candidate_index, 1:52]
 
         f = open('output_files/vote_counts/Numbers/' + candidates[candidate_index] + '.txt', 'w')
         f.write('Left Unity: ' + str(candidate_vote_counts1) + '\n')
@@ -91,22 +90,22 @@ def plot_vote_counts(cluster1, cluster2, cluster3, candidates):
         f.write('Unaligned ' + str(candidate_vote_counts3) + '\n')
         f.close()
 
-        bar1 = plt.bar(range(1, 26), candidate_vote_counts3, color="b")
-        bar2 = plt.bar(range(1, 26), candidate_vote_counts1, color="r")
-        bar3 = plt.bar(range(1, 26), candidate_vote_counts2, color="y")
+        bar1 = plt.bar(range(1, 52), candidate_vote_counts3, color="b")
+        bar2 = plt.bar(range(1, 52), candidate_vote_counts1, color="r")
+        bar3 = plt.bar(range(1, 52), candidate_vote_counts2, color="y")
 
         plt.legend((bar1, bar2, bar3), ("Unaligned", "Left Unity", "Momentum"))
         plt.title("Vote Distribution for " + candidates[candidate_index])
         plt.xlabel("Score Assigned")
         plt.ylabel("Number of Votes")
         plt.ylim((0, 85))
-        #plt.savefig('output_files/vote_counts/Images/' + candidates[candidate_index] + ".png")
+        plt.savefig('output_files/vote_counts/Images/' + candidates[candidate_index] + ".png")
         plt.clf()
 
 
 def print_pca_components(pca):
     for i in range(len(pca.components_)):
-        f = open('output_files/pca_components/component' + str(i) + '.txt', "w")
+        f = open('output_files/pca_components/textfiles/component' + str(i+1) + '.txt', "w")
         f.write(str(pca.components_[i]))
         f.close()
 
