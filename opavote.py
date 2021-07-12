56711fa0ecc11187eb17738fff7e786434a83816
Garett Miller <g.miller@elsevier.com>
2021-07-12 17:26:23 -0400

    Add OpaVote class and refactor

---
+++ opavote.py
@@ -0,0 +1,197 @@
+import numpy as np
+from scipy.spatial.distance import cdist
+from sklearn.decomposition import PCA
+from sklearn.cluster import KMeans
+
+import outputs
+
+
+class OpaVote:
+
+    def __init__(self, ballot_location, output_location):
+        self.ballot_location = ballot_location
+        self.output_location = output_location
+        self.cluster_labels = np.empty()
+        self.cluster_centers = np.empty()
+
+        self.ballots, self.candidates, self.num_candidates, self.num_seats = self.__get_ballots_and_candidates(ballot_location)
+        self.vectorized_ballots = self.__vectorize_matrix(self.ballots, self.num_candidates)
+        self.pca_ballots, self.pca = self.__do_pca(self.vectorized_ballots)
+
+    def __get_ballots_and_candidates(self, file_address):
+        """Get ballots and candidates as lists"""
+        lines, num_candidates, num_seats = self.__read_ballot_file(file_address)
+
+        ballots = [[0] * num_candidates for i in range(332)]
+        candidates = {}
+        is_ballot = True
+        ballot_num = 0
+        for ballot in lines:
+            if ballot == '0\n':
+                is_ballot = False
+                continue
+
+            if is_ballot:
+                ballot = ballot.replace('\n', '')
+                votes = [int(i) for i in ballot.split(' ')][1:-1]
+                if votes:
+                    ballots[ballot_num] = votes
+                    ballot_num += 1
+            else:
+                candidate = ballot.replace('"', '').replace('\n', '')
+                candidates[len(candidates)] = candidate
+
+        return ballots, candidates, num_candidates, num_seats
+
+    def __read_ballot_file(self, file_address):
+        ballot_file = open(file_address, "r")
+        lines = ballot_file.readlines()
+        # Remove num candidates and votes per ballot
+        first_line = lines[0].split(' ')
+        lines.remove(lines[0])
+        lines.remove(lines[1])
+        lines.remove(lines[-1])
+        return lines, int(first_line[0]), int(first_line[1])
+
+    def __vectorize_matrix(self, ballots, num_candidates):
+        """Transform ballots into 'rank' matrices. The first candidate voted for by a voter gets rank 44.
+        The last gets 1. Anyone not voted for gets 0"""
+        vectorized_ballots = [[0] * num_candidates for i in range(len(ballots))]
+
+        for ballot_num in range(len(ballots)):
+            ballot = ballots[ballot_num]
+            value = num_candidates
+            for vote_num in range(len(ballot)):
+                vectorized_ballots[ballot_num][ballot[vote_num] - 1] = value
+                value -= 1
+
+        return vectorized_ballots
+
+    def __do_pca(self, transformed_ballots):
+        array = np.array(transformed_ballots)
+        pca = PCA(n_components=15)
+        reduced_array = pca.fit_transform(array)
+        return reduced_array, pca
+
+    def __get_slate_keys(self, slate):
+        candidate_names = list(self.candidates.values())
+        return [[candidate_names.index(candidate) + 1 for candidate in slate[0]]]
+
+    def transform_slates(self, slates):
+        """Vectorize and perform PCA on slates"""
+        transformed_slates = np.empty(len(slates))
+
+        for i in range(len(slates)):
+            slate_keys = self.__get_slate_keys(slates[i])
+            vectorized_br_slate = self.__vectorize_matrix(slate_keys, self.num_candidates)
+            rotated_slate = self.pca.transform(vectorized_br_slate)
+            transformed_slates[i] = rotated_slate[0]
+
+        return transformed_slates
+
+    def plot_cluster_number_variances(self):
+        """Plot graph of explained variance vs number of nodes"""
+        cluster_variances = []
+        cluster_choices = range(1, 10)
+
+        for i in cluster_choices:
+            KMeans_model = KMeans(n_clusters=i, n_init=100).fit(self.pca_ballots)
+            cluster_variances.append(
+                100 - sum(np.min(cdist(self.pca_ballots, KMeans_model.cluster_centers_, 'euclidean'), axis=1)) /
+                self.pca_ballots.shape[0])
+
+        outputs.plot_cluster_number_variances(cluster_choices, cluster_variances)
+
+    def generate_clusters(self, num_clusters):
+        """Generate clusters using passed in number and save the labels and centers"""
+        kmeans = KMeans(n_clusters=num_clusters, n_init=100)
+        cluster_labels = kmeans.fit_predict(self.pca_ballots)
+
+        np.save("output_files/2021/clusters/labels.npy", cluster_labels)
+        np.save("output_files/2021/clusters/centers.npy", kmeans.cluster_centers_)
+
+        self.cluster_centers = kmeans.cluster_centers_
+        self.cluster_labels = cluster_labels
+
+    def load_clusters(self):
+        """Load in saved clusters"""
+        self.cluster_labels = np.load("output_files/2021/clusters/labels.npy")
+        self.cluster_centers = np.load("output_files/2021/clusters/centers.npy")
+
+    def write_cluster_centers(self, cluster_names):
+        """Write text file with pre-PCA location of cluster centers"""
+        outputs.write_cluster_centers(self.pca.inverse_transform(self.cluster_centers_), self.candidates, cluster_names)
+
+    def write_diff_cluster_centers(self, compared_clusters):
+        """Subtract one cluster location from another and write to text file so """
+        reverted_clusters = self.pca.inverse_transform(self.cluster_centers)
+        outputs.write_diff_cluster_centers(reverted_clusters, self.candidates, self.cluster_names, compared_clusters)
+
+    def query_ballots(self, query):
+        """Write text file with all ballots with candidates at the specified voting positions"""
+        outputs.ballot_query(query, self.ballots, self.cluster_labels, self.cluster_names, self.candidates)
+
+    def __split_by_cluster(self, ballots):
+        """Split ballots into separate indices in an array"""
+        clusters = []
+        for i in range(len(self.cluster_centers)):
+            index = [j for j, x in enumerate(self.cluster_labels) if x == i]
+            clustered_votes = np.array([list(ballots[j]) for j in index])
+            clusters.append(clustered_votes)
+
+        return clusters
+
+    def __get_cluster_counts(self):
+        """Get number of members in each cluster"""
+        cluster_counts = [0] * len(self.cluster_centers)
+
+        for cluster in self.cluster_labels:
+            cluster_counts[cluster] += 1
+
+        return cluster_counts
+
+    def plot_ballots_pca(self, pca_slates, x_pca_index, y_pca_index, x_pca_description, y_pca_description):
+        """Create 2 dimensional plots using the selected PCA components. Clusters are color-coded."""
+        clustered_pca_ballots = self.__split_by_cluster(self.pca_ballots)
+        outputs.plot_pca_ballots(np.array(clustered_pca_ballots), pca_slates, self.cluster_names, x_pca_index, y_pca_index, x_pca_description, y_pca_description)
+
+    def write_cluster_counts(self, cluster_names):
+        """Write text file with names of clusters and number of members."""
+        cluster_counts = self.__get_cluster_counts(self.cluster_labels, len(self.cluster_centers))
+        outputs.write_cluster_counts(cluster_counts, cluster_names)
+
+    def write_pca_variances(self):
+        """Write text file with PCA components and percent variance explained for each"""
+        outputs.write_pca_variances(self.pca)
+
+    def write_pca_components_by_candidate(self):
+        """Write text files with new PCA axes and each components representation on that axis"""
+        outputs.write_pca_components(self.pca, self.candidates)
+
+    def plot_important_pca_components(self, threshold=.16):
+        """Plot the most important axes of the import PCA components"""
+        outputs.plot_important_pca_components(self.pca, self.candidates, threshold)
+
+    def plot_candidate_vote_distributions(self, cluster_names):
+        """Calculate the number of votes for each candidate at each position and plot the distribution"""
+        clustered_ballots = self.__split_clusters(self.vectorized_ballots)
+        clustered_vote_counts = self.__get_clustered_vote_counts(np.array(clustered_ballots))
+        outputs.plot_candidate_vote_distributions(clustered_vote_counts, self.candidates, cluster_names)
+
+    def __get_clustered_vote_counts(self, clustered_ballots):
+        """Count the number of votes in each position for each candidate by the clustered ballots passed in.
+        Returns an array of clustered counts"""
+
+        return [self.__count_cluster_votes_for_candidate(np.array(ballots_in_cluster)) for ballots_in_cluster in clustered_ballots]
+
+    def __count_cluster_votes_for_candidate(self, ballots_in_cluster, ):
+        """Count the number of votes in each position for each candidate by the cluster of ballots passed in."""
+
+        counts = np.zeros((self.num_candidates, self.num_candidates + 1), dtype=np.int8)
+        for i in range(self.num_candidates):
+            votes_for_candidate = ballots_in_cluster[:, int(i)]
+            for vote in votes_for_candidate:
+                counts[i][vote] += 1
+
+        return counts
+
--- outputs.py
+++ outputs.py
@@ -3,16 +3,17 @@ import matplotlib.pyplot as plt
 
 def write_candidate_and_value(f, candidates, values):
     for i in range(len(candidates)):
-        f.write(candidates[i] + ': ' + str(values[i]) + '\n')
+        f.write(candidates[i] + ": " + str(values[i]) + "\n")
 
 
-def plot_important_components(pca, candidates):
+def plot_important_pca_components(pca, candidates, threshold):
+    """Plot the most important axes of the import PCA components"""
     for i in range(6):
         component = pca.components_[i]
         important_candidates = []
         important_values = []
         for j in range(len(component)):
-            if component[j] > .16 or component[j] < -.16:
+            if component[j] > threshold or component[j] < -threshold:
                 important_candidates.append(candidates[j])
                 important_values.append(component[j])
 
@@ -23,39 +24,43 @@ def plot_important_components(pca, candidates):
         plt.xlabel("Candidate")
         plt.ylabel("Candidate's Relative Alignment to Component")
 
-        plt.savefig('output_files/2021/pca_components/graphs/component' + str(i + 1) + '.png')
+        plt.savefig("output_files/2021/pca_components/graphs/component" + str(i + 1) + ".png")
 
 
-def plot_pca_ballots(clusters, rotated_slates, cluster_names):
+def plot_ballots_pca(clusters, pca_slates, cluster_names, x_pca_index, y_pca_index, x_pca_description, y_pca_description):
+    """Create 2 dimensional plots using the selected PCA components. Clusters are color-coded"""
     legend_labels = list(cluster_names.keys())
+    x_index_label = str(x_pca_index + 1)
+    y_index_label = str(y_pca_index + 1)
     groups = []
 
     for i in range(len(clusters)):
         color = list(cluster_names.values())[i]
-        group, = plt.plot(clusters[i][:, 0], clusters[i][:, 1], color + 'o')
+        group, = plt.plot(clusters[i][:, 0], clusters[i][:, 2], color + "o")
         groups.append(group)
 
     legend_labels.append("Slate Voting Guides")
-    slates, = plt.plot(rotated_slates[:, 0], rotated_slates[:, 1], 'k*')
+    slates, = plt.plot(pca_slates[:, 0], pca_slates[:, 2], "k*")
     groups.append(slates)
 
     plt.legend(groups, legend_labels)
-    plt.title("Voter Plot Principal Axes 1 and 2")
-    plt.xlabel("Principal Axis 1: LU vs. Momentum")
-    plt.ylabel("Principal Axis 3: Unsure")
-    plt.savefig('output_files/2021/vote_plots/PCA12.png')
+    plt.title("Voter Plot Principal Axes " + x_index_label + " and " + y_index_label)
+    plt.xlabel("Principal Axis " + x_index_label + ": " + x_pca_description) #LU vs. Momentum
+    plt.ylabel("Principal Axis " + y_index_label + ": " + y_pca_description) #Unsure
+    plt.savefig("output_files/2021/vote_plots/PCA" + x_index_label + y_index_label + ".png")
     plt.clf()
 
 
-def plot_vote_counts(clusters, candidates, cluster_names):
+def plot_candidate_vote_distributions(clusters, candidates, cluster_names):
+    """Plot graph and write text file of each candidate with a cluster coded distribution of votes (vectorized)"""
     for candidate_index in range(len(candidates)):
         clustered_vote_counts = [cluster[candidate_index, 1:45] for cluster in clusters]
 
         clustered_bars = []
-        f = open('output_files/2021/vote_counts/numbers/' + candidates[candidate_index] + '.txt', 'w')
+        f = open("output_files/2021/vote_counts/numbers/" + candidates[candidate_index] + ".txt", "w")
         bottom = [0] * 44
         for i in range(len(clustered_vote_counts)):
-            f.write(list(cluster_names.keys())[i] + ': ' + str(clustered_vote_counts[i]) + '\n')
+            f.write(list(cluster_names.keys())[i] + ": " + str(clustered_vote_counts[i]) + "\n")
 
             bar = plt.bar(range(1, 45), clustered_vote_counts[i], bottom=bottom, color=list(cluster_names.values())[i])
             bottom = bottom + clustered_vote_counts[i]
@@ -68,44 +73,51 @@ def plot_vote_counts(clusters, candidates, cluster_names):
         plt.xlabel("Score Assigned")
         plt.ylabel("Number of Votes")
         plt.ylim((0, 130))
-        plt.savefig('output_files/2021/vote_counts/images/' + candidates[candidate_index] + ".png")
+        plt.savefig("output_files/2021/vote_counts/images/" + candidates[candidate_index] + ".png")
         plt.clf()
 
 
-def write_pca_components(pca, candidates):
+def write_pca_components_by_candidate(pca, candidates):
+    """Write text files with new PCA axes and each components representation on that axis"""
     for i in range(len(pca.components_)):
-        f = open('output_files/2021/pca_components/textfiles/component' + str(i+1) + '.txt', "w")
-        f.write("PCA Component " + str(i + 1) + '\n\n')
+        f = open("output_files/2021/pca_components/textfiles/component" + str(i+1) + ".txt", "w")
+        f.write("PCA Component " + str(i + 1) + "\n\n")
         write_candidate_and_value(f, candidates, pca.components_[i])
         f.close()
 
 
-def print_totals(pca, cluster_counts, cluster_names):
-    f = open('output_files/2021/totals.txt', 'w')
+def write_cluster_counts(cluster_counts, cluster_names):
+    """Write text file with names of clusters and number of members."""
+    f = open("output_files/2021/cluster_counts.txt", "w")
 
-    f.write('Ballot Analysis Totals')
-    f.write('\n\nCluster Counts\n')
+    f.write("Ballot Analysis Totals")
+    f.write("\n\nCluster Counts\n")
     for cluster in range(len(cluster_counts)):
-        f.write('Voters in ' + list(cluster_names.keys())[cluster] + ': ' + str(cluster_counts[cluster]) + '\n')
+        f.write("Voters in " + list(cluster_names.keys())[cluster] + ": " + str(cluster_counts[cluster]) + "\n")
 
-    f.write('\n\n\nExplained Variance Ratios by Component\n')
+
+def write_pca_variances(pca):
+    """Write text file with PCA components and percent variance explained for each"""
+    f = open("output_files/2021/pca_variances.txt", "w")
+
+    f.write("\n\n\nExplained Variance Ratios by Component\n")
     for component in range(len(pca.explained_variance_ratio_)):
-        f.write('Component ' + str(component + 1) + ': ' + str(pca.explained_variance_ratio_[component]) + '\n')
+        f.write("Component " + str(component + 1) + ": " + str(pca.explained_variance_ratio_[component]) + "\n")
     f.close()
 
 
 def write_cluster_centers(cluster_centers, candidates, cluster_names):
     for i in range(len(cluster_centers)):
-        f = open('output_files/2021/clusters/cluster' + str(i + 1) + '.txt', "w")
-        f.write(list(cluster_names.keys())[i] + ' Cluster Location\n\n')
+        f = open("output_files/2021/clusters/cluster" + str(i + 1) + ".txt", "w")
+        f.write(list(cluster_names.keys())[i] + " Cluster Location\n\n")
         write_candidate_and_value(f, candidates, cluster_centers[i])
         f.close()
 
 
 def write_diff_cluster_centers(cluster_centers, candidates, cluster_names, compared_clusters):
-    f = open('output_files/2021/clusters/diffs/clusterdiff' + str(compared_clusters[0] + 1) + str(compared_clusters[1] + 1) + '.txt', "w")
+    f = open("output_files/2021/clusters/diffs/clusterdiff" + str(compared_clusters[0] + 1) + str(compared_clusters[1] + 1) + ".txt", "w")
     cluster_name_keys = list(cluster_names.keys())
-    f.write(cluster_name_keys[compared_clusters[0]] + ' minus '+ cluster_name_keys[compared_clusters[1]] + ' Cluster Diff\n\n')
+    f.write(cluster_name_keys[compared_clusters[0]] + " minus "+ cluster_name_keys[compared_clusters[1]] + " Cluster Diff\n\n")
     write_candidate_and_value(f, candidates, cluster_centers[compared_clusters[0]] - cluster_centers[compared_clusters[1]])
     f.close()
 
@@ -115,8 +127,8 @@ def get_ballot_string(ballot, candidates):
 
 
 def ballot_query(query_dictionary, ballots, cluster_labels, cluster_names, candidates):
-    file_name = 'output_files/2021/queries/'
-    description_string = ''
+    file_name = "output_files/2021/queries/"
+    description_string = ""
     candidate_names = list(candidates.values())
     selected_voter_indices = []
 
@@ -133,13 +145,20 @@ def ballot_query(query_dictionary, ballots, cluster_labels, cluster_names, candi
                 selected_voter_indices.append(i)
 
         file_name = file_name + name + str(query_dictionary[name])
-        description_string = name + ' at position ' + str(query_dictionary[name]) + ' and '
+        description_string = name + " at position " + str(query_dictionary[name]) + " and "
 
-    file_name = file_name + '.txt'
+    file_name = file_name + ".txt"
     f = open(file_name, "w")
 
-    f.write('This file contains all voters who voted for ' + description_string + '\n\n')
+    f.write("This file contains all voters who voted for " + description_string + "\n\n")
     for i in selected_voter_indices:
-        f.write('Voter ' + str(i) + ' in ' + list(cluster_names.keys())[cluster_labels[i]] + ': ' + get_ballot_string(ballots[i], candidates) + '\n')
+        f.write("Voter " + str(i) + " in " + list(cluster_names.keys())[cluster_labels[i]] + ": " + get_ballot_string(ballots[i], candidates) + "\n")
 
+
+def plot_cluster_number_variances(cluster_choices, cluster_variances) :
+    plt.plot(cluster_choices, cluster_variances, "bx-")
+    plt.xlabel("Number of Nodes")
+    plt.ylabel("Variance Explained")
+    plt.title("The Elbow Method showing the optimal k")
+    plt.savefig("output_files/2021/node_numbers.png")
 
