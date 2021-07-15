b3ba9cbe5daf54a78ac9a2da718ad304fb9d04a9
Garett Miller <g.miller@elsevier.com>
2021-07-15 15:48:57 -0400

    Moved file system control to Opavote class

--- opavote.py
+++ opavote.py
@@ -1,5 +1,5 @@
 import numpy as np
-import os
+from pathlib import Path
 from scipy.spatial.distance import cdist
 from sklearn.decomposition import PCA
 from sklearn.cluster import KMeans
@@ -12,6 +12,7 @@ class OpaVote:
     def __init__(self, ballot_location, output_location):
         self.ballot_location = ballot_location
         self.output_location = output_location
+        Path(output_location).mkdir(parents=True, exist_ok=True)
 
         self.ballots, self.candidates, self.num_candidates, self.num_seats = self.__read_ballots_and_candidates(ballot_location)
         self.vectorized_ballots = self.__vectorize_matrix(self.ballots, self.num_candidates)
@@ -93,23 +94,34 @@ class OpaVote:
 
     def write_pca_variances(self):
         """Write text file with PCA components and percent variance explained for each"""
-        outputs.write_pca_variances(self.pca, self.output_location)
+        file_location = self.output_location + "pca_variances.txt"
+        outputs.write_pca_variances(self.pca, file_location)
 
     def write_pca_components_by_candidate(self):
         """Write text files with new PCA axes and each components representation on that axis"""
-        outputs.write_pca_components_by_candidate(self.pca, self.candidates, self.output_location)
+        file_dir = self.output_location + "pca_components/textfiles/"
+        Path(file_dir).mkdir(parents=True, exist_ok=True)
+        file_locations = [file_dir + "component" + str(i + 1) + ".txt" for i in range(len(self.pca.components_))]
 
-    def plot_important_pca_components(self, threshold=.16):
+        outputs.write_pca_components_by_candidate(self.pca, list(self.candidates.values()), file_locations)
+
+    def plot_important_pca_components(self, threshold=.16, num_components=6):
         """Plot the most important axes of the import PCA components"""
-        outputs.plot_important_pca_components(self.pca, self.candidates, threshold, self.output_location)
+        file_dir = self.output_location + "pca_components/graphs/"
+        Path(file_dir).mkdir(parents=True, exist_ok=True)
+        file_locations = [file_dir + "component" + str(i + 1) + ".png" for i in range(num_components)]
+
+        outputs.plot_important_pca_components(self.pca, list(self.candidates.values()), threshold, file_locations)
 
     def generate_clusters(self, num_clusters):
         """Generate clusters using passed in number and save the labels and centers"""
         kmeans = KMeans(n_clusters=num_clusters, n_init=100)
         cluster_labels = kmeans.fit_predict(self.pca_ballots)
 
-        np.save(self.output_location + "clusters/labels.npy", cluster_labels)
-        np.save(self.output_location + "clusters/centers.npy", kmeans.cluster_centers_)
+        file_dir = self.output_location + "clusters/"
+        Path(file_dir).mkdir(parents=True, exist_ok=True)
+        np.save(file_dir + "labels.npy", cluster_labels)
+        np.save(file_dir + "centers.npy", kmeans.cluster_centers_)
 
         self.cluster_centers = kmeans.cluster_centers_
         self.cluster_labels = cluster_labels
@@ -121,15 +133,24 @@ class OpaVote:
 
     def write_cluster_centers(self, cluster_names):
         """Write text file with pre-PCA location of cluster centers"""
-        outputs.write_cluster_centers(self.pca.inverse_transform(self.cluster_centers), self.candidates, cluster_names, self.output_location)
+        file_dir = self.output_location + "clusters/"
+        Path(file_dir).mkdir(parents=True, exist_ok=True)
+        file_locations = [file_dir + "cluster" + str(i + 1) + ".txt" for i in range(len(self.cluster_centers))]
+
+        outputs.write_cluster_centers(self.pca.inverse_transform(self.cluster_centers), list(self.candidates.values()), cluster_names, file_locations)
 
     def write_diff_cluster_centers(self, cluster_names, compared_clusters):
         """Subtract one cluster location from another and write to text file so clusters can be compared"""
+        file_location = self.output_location + "clusters/diffs/clusterdiff" + str(compared_clusters[0] + 1) + str(
+            compared_clusters[1] + 1) + ".txt"
+
         reverted_clusters = self.pca.inverse_transform(self.cluster_centers)
-        outputs.write_diff_cluster_centers(reverted_clusters, self.candidates, cluster_names, compared_clusters, self.output_location)
+        outputs.write_diff_cluster_centers(reverted_clusters, list(self.candidates.values()), cluster_names, compared_clusters, file_location)
 
     def plot_cluster_number_variances(self):
         """Plot graph of explained variance vs number of nodes"""
+        file_location = self.output_location + "node_numbers.png"
+
         cluster_variances = []
         cluster_choices = range(1, 10)
 
@@ -139,7 +160,7 @@ class OpaVote:
                 100 - sum(np.min(cdist(self.pca_ballots, KMeans_model.cluster_centers_, 'euclidean'), axis=1)) /
                 self.pca_ballots.shape[0])
 
-        outputs.plot_cluster_number_variances(cluster_choices, cluster_variances, self.output_location)
+        outputs.plot_cluster_number_variances(cluster_choices, cluster_variances, file_location)
 
     def __split_by_cluster(self, ballots):
         """Split ballots into separate indices in an array"""
@@ -162,19 +183,33 @@ class OpaVote:
 
     def plot_ballots_pca(self, pca_slates, cluster_names, x_pca_index, y_pca_index, x_pca_description, y_pca_description):
         """Create 2 dimensional plots using the selected PCA components. Clusters are color-coded."""
+        file_dir = self.output_location + "vote_plots/"
+        Path(file_dir).mkdir(parents=True, exist_ok=True)
+        file_location = file_dir + "vote_plots/PCA" + str(x_pca_index) + str(y_pca_index) + ".png"
+
         clustered_pca_ballots = self.__split_by_cluster(self.pca_ballots)
-        outputs.plot_ballots_pca(np.array(clustered_pca_ballots), pca_slates, cluster_names, x_pca_index, y_pca_index, x_pca_description, y_pca_description, self.output_location)
+        outputs.plot_ballots_pca(np.array(clustered_pca_ballots), pca_slates, cluster_names, x_pca_index, y_pca_index, x_pca_description, y_pca_description, file_location)
 
     def write_cluster_counts(self, cluster_names):
         """Write text file with names of clusters and number of members."""
+        file_location = self.output_location + "cluster_counts.txt"
+
         cluster_counts = self.__get_cluster_counts()
-        outputs.write_cluster_counts(cluster_counts, cluster_names, self.output_location)
+        outputs.write_cluster_counts(cluster_counts, cluster_names, file_location)
 
     def plot_candidate_vote_distributions(self, cluster_names):
         """Calculate the number of votes for each candidate at each position and plot the distribution"""
+        text_file_dir = self.output_location + "vote_counts/textfiles/"
+        Path(text_file_dir).mkdir(parents=True, exist_ok=True)
+        text_file_locations = [text_file_dir + + candidate + ".txt" for candidate in list(self.candidates.values())]
+
+        graph_file_dir = self.output_location + "vote_counts/images/"
+        Path(graph_file_dir).mkdir(parents=True, exist_ok=True)
+        graph_file_locations = [graph_file_dir + candidate + ".png" for candidate in list(self.candidates.values())]
+
         clustered_ballots = self.__split_by_cluster(self.vectorized_ballots)
         clustered_vote_counts = self.__get_clustered_vote_counts(np.array(clustered_ballots))
-        outputs.plot_candidate_vote_distributions(clustered_vote_counts, self.candidates, cluster_names, self.output_location)
+        outputs.plot_candidate_vote_distributions(clustered_vote_counts, list(self.candidates.values()), cluster_names, text_file_locations, graph_file_locations)
 
     def __get_clustered_vote_counts(self, clustered_ballots):
         """Count the number of votes in each position for each candidate by the clustered ballots passed in.
@@ -182,7 +217,7 @@ class OpaVote:
 
         return [self.__count_cluster_votes_for_candidate(np.array(ballots_in_cluster)) for ballots_in_cluster in clustered_ballots]
 
-    def __count_cluster_votes_for_candidate(self, ballots_in_cluster, ):
+    def __count_cluster_votes_for_candidate(self, ballots_in_cluster):
         """Count the number of votes in each position for each candidate by the cluster of ballots passed in."""
 
         counts = np.zeros((self.num_candidates, self.num_candidates + 1), dtype=np.int8)
@@ -195,5 +230,26 @@ class OpaVote:
 
     def query_ballots(self, query, cluster_names):
         """Write text file with all ballots with candidates at the specified voting positions"""
-        outputs.ballot_query(query, self.ballots, self.cluster_labels, cluster_names, self.candidates, self.output_location)
+        file_location = self.output_location + "queries/"
+        description_string = ""
+        candidate_names = list(self.candidates.values())
+        selected_voter_indices = []
+
+        for name in list(query.keys()):
+            candidate_index = 0
+
+            for i in range(len(candidate_names)):
+                if name == candidate_names[i]:
+                    candidate_index = i + 1
+                    break
+
+            for i in range(len(self.ballots)):
+                if len(self.ballots[i]) > query[name] and self.ballots[i][query[name]] == candidate_index:
+                    selected_voter_indices.append(i)
+
+            file_location = file_location + name + str(query[name])
+            description_string = name + " at position " + str(query[name]) + " and "
+
+        file_location = file_location + ".txt"
+        outputs.write_queried_ballots(description_string, selected_voter_indices, self.ballots, candidate_names, self.cluster_labels, cluster_names, file_location)
 
--- outputs.py
+++ outputs.py
@@ -6,9 +6,9 @@ def write_candidate_and_value(f, candidates, values):
         f.write(candidates[i] + ": " + str(values[i]) + "\n")
 
 
-def plot_important_pca_components(pca, candidates, threshold, output_location):
+def plot_important_pca_components(pca, candidates, threshold, file_locations):
     """Plot the most important axes of the import PCA components"""
-    for i in range(6):
+    for i in range(len(file_locations)):
         component = pca.components_[i]
         important_candidates = []
         important_values = []
@@ -24,10 +24,63 @@ def plot_important_pca_components(pca, candidates, threshold, output_location):
         plt.xlabel("Candidate")
         plt.ylabel("Candidate's Relative Alignment to Component")
 
-        plt.savefig(output_location + "pca_components/graphs/component" + str(i + 1) + ".png")
+        plt.savefig(file_locations[i])
 
 
-def plot_ballots_pca(clusters, pca_slates, cluster_names, x_pca_index, y_pca_index, x_pca_description, y_pca_description, output_location):
+def write_pca_components_by_candidate(pca, candidates, file_locations):
+    """Write text files with new PCA axes and each components representation on that axis"""
+    for i in range(len(file_locations)):
+        f = open(file_locations, "w")
+        f.write("PCA Component " + str(i + 1) + "\n\n")
+        write_candidate_and_value(f, candidates, pca.components_[i])
+        f.close()
+
+
+def write_pca_variances(pca, file_location):
+    """Write text file with PCA components and percent variance explained for each"""
+    f = open(file_location, "w")
+
+    f.write("\n\n\nExplained Variance Ratios by Component\n")
+    for component in range(len(pca.explained_variance_ratio_)):
+        f.write("Component " + str(component + 1) + ": " + str(pca.explained_variance_ratio_[component]) + "\n")
+    f.close()
+
+
+def plot_cluster_number_variances(cluster_choices, cluster_variances, file_location):
+    plt.plot(cluster_choices, cluster_variances, "bx-")
+    plt.xlabel("Number of Nodes")
+    plt.ylabel("Variance Explained")
+    plt.title("The Elbow Method showing the optimal k")
+    plt.savefig(file_location)
+
+
+def write_cluster_counts(cluster_counts, cluster_names, file_location):
+    """Write text file with names of clusters and number of members."""
+    f = open(file_location, "w")
+
+    f.write("Ballot Analysis Totals")
+    f.write("\n\nCluster Counts\n")
+    for cluster in range(len(cluster_counts)):
+        f.write("Voters in " + list(cluster_names.keys())[cluster] + ": " + str(cluster_counts[cluster]) + "\n")
+
+
+def write_cluster_centers(cluster_centers, candidates, cluster_names, file_locations):
+    for i in range(len(file_locations)):
+        f = open(file_locations, "w")
+        f.write(list(cluster_names.keys())[i] + " Cluster Location\n\n")
+        write_candidate_and_value(f, candidates, cluster_centers[i])
+        f.close()
+
+
+def write_diff_cluster_centers(cluster_centers, candidates, cluster_names, compared_clusters, file_location):
+    f = open(file_location, "w")
+    cluster_name_keys = list(cluster_names.keys())
+    f.write(cluster_name_keys[compared_clusters[0]] + " minus "+ cluster_name_keys[compared_clusters[1]] + " Cluster Diff\n\n")
+    write_candidate_and_value(f, candidates, cluster_centers[compared_clusters[0]] - cluster_centers[compared_clusters[1]])
+    f.close()
+
+
+def plot_ballots_pca(clusters, pca_slates, cluster_names, x_pca_index, y_pca_index, x_pca_description, y_pca_description, file_location):
     """Create 2 dimensional plots using the selected PCA components. Clusters are color-coded"""
     legend_labels = list(cluster_names.keys())
     x_index_label = str(x_pca_index + 1)
@@ -47,17 +100,17 @@ def plot_ballots_pca(clusters, pca_slates, cluster_names, x_pca_index, y_pca_ind
     plt.title("Voter Plot Principal Axes " + x_index_label + " and " + y_index_label)
     plt.xlabel("Principal Axis " + x_index_label + ": " + x_pca_description)
     plt.ylabel("Principal Axis " + y_index_label + ": " + y_pca_description)
-    plt.savefig(output_location + "vote_plots/PCA" + x_index_label + y_index_label + ".png")
+    plt.savefig(file_location)
     plt.clf()
 
 
-def plot_candidate_vote_distributions(clusters, candidates, cluster_names, output_location):
+def plot_candidate_vote_distributions(clusters, candidates, cluster_names, text_file_locations, graph_file_locations):
     """Plot graph and write text file of each candidate with a cluster coded distribution of votes (vectorized)"""
     for candidate_index in range(len(candidates)):
         clustered_vote_counts = [cluster[candidate_index, 1:45] for cluster in clusters]
 
         clustered_bars = []
-        f = open(output_location + "vote_counts/numbers/" + candidates[candidate_index] + ".txt", "w")
+        f = open(text_file_locations[candidate_index], "w")
         bottom = [0] * 44
         for i in range(len(clustered_vote_counts)):
             f.write(list(cluster_names.keys())[i] + ": " + str(clustered_vote_counts[i]) + "\n")
@@ -73,92 +126,19 @@ def plot_candidate_vote_distributions(clusters, candidates, cluster_names, outpu
         plt.xlabel("Score Assigned")
         plt.ylabel("Number of Votes")
         plt.ylim((0, 130))
-        plt.savefig(output_location + "vote_counts/images/" + candidates[candidate_index] + ".png")
+        plt.savefig(graph_file_locations[candidate_index])
         plt.clf()
-
-
-def write_pca_components_by_candidate(pca, candidates, output_location):
-    """Write text files with new PCA axes and each components representation on that axis"""
-    for i in range(len(pca.components_)):
-        f = open(output_location + "pca_components/textfiles/component" + str(i+1) + ".txt", "w")
-        f.write("PCA Component " + str(i + 1) + "\n\n")
-        write_candidate_and_value(f, candidates, pca.components_[i])
-        f.close()
-
-
-def write_cluster_counts(cluster_counts, cluster_names, output_location):
-    """Write text file with names of clusters and number of members."""
-    f = open(output_location + "cluster_counts.txt", "w")
-
-    f.write("Ballot Analysis Totals")
-    f.write("\n\nCluster Counts\n")
-    for cluster in range(len(cluster_counts)):
-        f.write("Voters in " + list(cluster_names.keys())[cluster] + ": " + str(cluster_counts[cluster]) + "\n")
-
-
-def write_pca_variances(pca, output_location):
-    """Write text file with PCA components and percent variance explained for each"""
-    f = open(output_location + "pca_variances.txt", "w")
-
-    f.write("\n\n\nExplained Variance Ratios by Component\n")
-    for component in range(len(pca.explained_variance_ratio_)):
-        f.write("Component " + str(component + 1) + ": " + str(pca.explained_variance_ratio_[component]) + "\n")
-    f.close()
-
-
-def write_cluster_centers(cluster_centers, candidates, cluster_names, output_location):
-    for i in range(len(cluster_centers)):
-        f = open(output_location + "clusters/cluster" + str(i + 1) + ".txt", "w")
-        f.write(list(cluster_names.keys())[i] + " Cluster Location\n\n")
-        write_candidate_and_value(f, candidates, cluster_centers[i])
-        f.close()
-
-
-def write_diff_cluster_centers(cluster_centers, candidates, cluster_names, compared_clusters, output_location):
-    f = open(output_location + "clusters/diffs/clusterdiff" + str(compared_clusters[0] + 1) + str(compared_clusters[1] + 1) + ".txt", "w")
-    cluster_name_keys = list(cluster_names.keys())
-    f.write(cluster_name_keys[compared_clusters[0]] + " minus "+ cluster_name_keys[compared_clusters[1]] + " Cluster Diff\n\n")
-    write_candidate_and_value(f, candidates, cluster_centers[compared_clusters[0]] - cluster_centers[compared_clusters[1]])
-    f.close()
 
 
 def get_ballot_string(ballot, candidates):
     return str([candidates[i - 1] for i in ballot])
 
 
-def ballot_query(query_dictionary, ballots, cluster_labels, cluster_names, candidates, output_location):
-    file_name = output_location + "queries/"
-    description_string = ""
-    candidate_names = list(candidates.values())
-    selected_voter_indices = []
-
-    for name in list(query_dictionary.keys()):
-        candidate_index = 0
-
-        for i in range(len(candidate_names)):
-            if name == candidate_names[i]:
-                candidate_index = i + 1
-                break
-        
-        for i in range(len(ballots)):
-            if len(ballots[i]) > query_dictionary[name] and  ballots[i][query_dictionary[name]] == candidate_index:
-                selected_voter_indices.append(i)
-
-        file_name = file_name + name + str(query_dictionary[name])
-        description_string = name + " at position " + str(query_dictionary[name]) + " and "
-
-    file_name = file_name + ".txt"
-    f = open(file_name, "w")
+def write_queried_ballots(description_string, selected_voter_indices, ballots, candidates, cluster_labels, cluster_names, file_location):
+    f = open(file_location, "w")
 
     f.write("This file contains all voters who voted for " + description_string + "\n\n")
     for i in selected_voter_indices:
         f.write("Voter " + str(i) + " in " + list(cluster_names.keys())[cluster_labels[i]] + ": " + get_ballot_string(ballots[i], candidates) + "\n")
 
-
-def plot_cluster_number_variances(cluster_choices, cluster_variances, output_location):
-    plt.plot(cluster_choices, cluster_variances, "bx-")
-    plt.xlabel("Number of Nodes")
-    plt.ylabel("Variance Explained")
-    plt.title("The Elbow Method showing the optimal k")
-    plt.savefig(output_location + "node_numbers.png")
 
